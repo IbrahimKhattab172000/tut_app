c0af15c845b1c7ad3ddd04948363512752c74b31
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2022-08-28 07:36:28 +0200

    adding card theme

--- lib/presentation/themes_manager.dart
+++ lib/presentation/themes_manager.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:tut_app/presentation/values_manager.dart';
 
 import 'colors_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      // main colors
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.lightPrimary,
-      primaryColorDark: ColorManager.darkPrimary,
-      disabledColor: ColorManager.grey1
-      // cardview theme
+    // main colors
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.lightPrimary,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    // cardview theme
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
+    // app bar theme
 
-      // app bar theme
+    // button theme
 
-      // button theme
+    // text theme
 
-      // text theme
-
-      // input decoration theme (text form field)
-
-      );
+    // input decoration theme (text form field)
+  );
 }
