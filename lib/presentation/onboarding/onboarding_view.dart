08c47cd0d83ba4082b49597d0756f69b61524cd3
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2022-10-06 07:12:41 +0200

    adding logic to the onboarding view

--- lib/presentation/onboarding/onboarding_view.dart
+++ lib/presentation/onboarding/onboarding_view.dart
@@ -1,7 +1,14 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:tut_app/presentation/resources/assets_manager.dart';
 import 'package:tut_app/presentation/resources/colors_manager.dart';
+import 'package:tut_app/presentation/resources/strings_manager.dart';
+import 'package:tut_app/presentation/resources/values_manager.dart';
 
 class OnboardingView extends StatefulWidget {
   const OnboardingView({Key? key}) : super(key: key);
@@ -11,13 +18,119 @@ class OnboardingView extends StatefulWidget {
 }
 
 class _OnboardingViewState extends State<OnboardingView> {
+  int _currentIndex = 0;
+
+  late List<SliderObject> _list = _getSliderData();
+
+  final PageController _pageController = PageController(initialPage: 0);
+
+  List<SliderObject> _getSliderData() => [
+        SliderObject(
+          AppStrings.onBoardingTitle1,
+          AppStrings.onBoardingSubTitle1,
+          ImageAssets.onboardingLogo1,
+        ),
+        SliderObject(
+          AppStrings.onBoardingTitle2,
+          AppStrings.onBoardingSubTitle2,
+          ImageAssets.onboardingLogo2,
+        ),
+        SliderObject(
+          AppStrings.onBoardingTitle3,
+          AppStrings.onBoardingSubTitle3,
+          ImageAssets.onboardingLogo3,
+        ),
+        SliderObject(
+          AppStrings.onBoardingTitle4,
+          AppStrings.onBoardingSubTitle4,
+          ImageAssets.onboardingLogo4,
+        ),
+      ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManager.white,
-      body: Center(
-        child: Text("Welcome"),
+      appBar: AppBar(
+        // elevation: AppSize.s1_5,
+        elevation: 0,
+        backgroundColor: ColorManager.white,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: ColorManager.white,
+          statusBarBrightness: Brightness.dark,
+          statusBarIconBrightness: Brightness.dark,
+        ),
+      ),
+      body: PageView.builder(
+        controller: _pageController,
+        itemCount: _list.length,
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        itemBuilder: (context, index) {
+          return OnBoardingPage(
+            //*Approach1
+            //_list is a type List<SliderObject> but _list[index] is a SliderObject
+            sliderObject: _list[index],
+            //*Approach2
+            // sliderObject: SliderObject(
+            //   _list[index].title,
+            //   _list[index].subtitle,
+            //   _list[index].image,
+            // ),
+          );
+        },
       ),
     );
   }
+}
+
+class OnBoardingPage extends StatelessWidget {
+  final SliderObject sliderObject;
+  const OnBoardingPage({
+    Key? key,
+    required this.sliderObject,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: AppPadding.p14),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: AppSize.s40),
+          Padding(
+            padding: const EdgeInsets.all(AppPadding.p8),
+            child: Text(
+              sliderObject.title,
+              style: Theme.of(context).textTheme.headline1,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(AppPadding.p8),
+            child: Text(
+              sliderObject.subtitle,
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+          ),
+          SizedBox(height: AppSize.s60),
+          SvgPicture.asset(sliderObject.image),
+        ],
+      ),
+    );
+  }
+}
+
+class SliderObject {
+  String title;
+  String subtitle;
+  String image;
+  SliderObject(
+    this.title,
+    this.subtitle,
+    this.image,
+  );
 }
