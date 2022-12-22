a6f09f167ea0c85ecab21fbf1d37ff5c9e43748b
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2022-12-22 09:44:29 +0200

    added repo for request

---
+++ lib/data/request/request.dart
@@ -0,0 +1,12 @@
+class LoginRequest {
+  String email;
+  String password;
+  String imie;
+  String deviceType;
+  LoginRequest(
+    this.email,
+    this.password,
+    this.imie,
+    this.deviceType,
+  );
+}
