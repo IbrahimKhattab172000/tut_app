76e3f0babf422507400b2009b70087aff28a70ff
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2023-01-09 09:18:32 +0200

    added remote data source

--- lib/data/network/app_api.dart
+++ lib/data/network/app_api.dart
@@ -8,5 +8,10 @@ part 'app_api.g.dart';
 abstract class AppServiceClient {
   factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;
   @POST("/customers/login")
-  Future<AuthenticationResponse> login();
+  Future<AuthenticationResponse> login(
+    @Field("email") String email,
+    @Field("password") String password,
+    @Field("imie") String imie,
+    @Field("deviceType") String deviceType,
+  );
 }
